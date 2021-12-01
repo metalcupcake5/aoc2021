09eb736d3551e0a3c2eb94d4bc5c67c4a1dabd7a
Sophie <me@soopy.moe>
2021-12-01 23:38:05 +0800

    feat(soopyc): day 1 part 1 solution

--- soopyc/aoc/day1/a.py
+++ soopyc/aoc/day1/a.py
@@ -1,7 +1,46 @@
+from types import SimpleNamespace
+from collections import deque
+
+from rich.table import Table
+
 from .input import _string
 from ..tools.console import console
-print = console.print
+print = console.log
+
+
+class Changes(SimpleNamespace):
+    pass
 
 
 def main():
-    ...
+    counter = Changes(inc=0, dec=0, nc=0)
+    queue = deque(maxlen=2)
+    input_ = _string.decode()
+    data = input_.splitlines()
+    queue.append(int(data.pop(0)))
+    sign = ""
+
+    for count, i in enumerate(data):
+        queue.append(int(i))
+        if queue[1] > queue[0]:
+            counter.inc += 1
+            sign = ">"
+        elif queue[1] < queue[0]:
+            counter.dec += 1
+            sign = "<"
+        elif queue[1] == queue[0]:
+            counter.nc += 1
+            sign = "="
+
+        if count % 5 == 0:
+            print(f'#{count}: {queue[1]} {sign} {queue[0]}; {counter}; {round((count+1)/len(data)*100, 2)}%')
+
+    print(f'#{count}: {queue[1]} {sign} {queue[0]}; {counter}; {round((count+1)/len(data)*100, 2)}%')
+    # Summing up
+    table = Table("", "", title="Summary table")
+    table.add_row("Increases", str(counter.inc), style="green")
+    table.add_row("Decreases", str(counter.dec), style="red")
+    table.add_row("No Change", str(counter.nc), style="bold black")
+    table.add_row()
+    table.add_row("Grand Total", str(counter.inc + counter.dec + counter.nc), style="bold blue")
+    print(table)
